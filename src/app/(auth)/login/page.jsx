cc6034b28ac17b24d5e47e74b694e063e738f0a9
Your Name <babulhossan.info@gmail.com>
2026-04-25 20:13:45 +0600

    login and register setup

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,9 +1,65 @@
+"use client"
+import Link from 'next/link';
 import React from 'react';
+import { useForm } from 'react-hook-form';
+// import { useForm, SubmitHandler } from "react-hook-form"
+
 
 const LoginPage = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const handleLogin = (data) => {
+        console.log(data, errors, "data");
+
+    }
     return (
         <div>
-            login page  
+
+            <form onSubmit={handleSubmit(handleLogin)} className="container mx-auto pt-15 bg-base-200 min-h-screen">
+                <div className="hero-content flex-col lg:flex-row-reverse mt-15 ">
+
+                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                        <div className="card-body">
+                            <div>
+                                <h1 className="text-xl font-bold">Login your account</h1>
+                            </div>
+
+                            <fieldset className="fieldset">
+                                <label className=" text-lg font-semibold">Email address</label>
+
+                                <input type="email" className="input" placeholder="Email"
+                                    {...register("email", { required: true })}
+                                />
+
+                                {errors.email && (
+                                    <p className="text-red-500 text-sm">
+                                        Email is required
+                                    </p>
+                                )}
+
+                                <label className="text-lg font-semibold">Password</label>
+
+                                <input type="password" className="input" placeholder="Password"
+                                    {...register("password", { required: true })}
+
+                                />
+
+                                {errors.password && (
+                                    <p className="text-red-500 text-sm">
+                                        Password is required
+                                    </p>
+                                )}
+
+                                <div><a className="link link-hover">Forgot password?</a></div>
+                                <button className="btn btn-neutral mt-4">Login</button>
+                            </fieldset>
+                        </div>
+
+                        <p className='text-center pb-6'>Dont’t Have An Account ? <Link href={"/register"} className='text-red-700'>Register</Link></p>
+                    </div>
+                </div>
+            </form>
+
         </div>
     );
 };
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,11 +1,102 @@
+"use client"
+import Link from 'next/link';
 import React from 'react';
+import { useForm } from 'react-hook-form';
+// import { useForm, SubmitHandler } from "react-hook-form"
 
-const RegisterPage = () => {
+
+const LoginPage = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const handleLogin = (data) => {
+        console.log(data, errors, "data");
+
+    }
     return (
         <div>
-            register page
+
+            <form onSubmit={handleSubmit(handleLogin)} className="container mx-auto pt-15 bg-base-200 min-h-screen">
+                <div className="hero-content flex-col lg:flex-row-reverse mt-15 ">
+
+                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                        <div className="card-body">
+                            <div>
+                                <h1 className="text-xl font-bold">Register your account</h1>
+                            </div>
+
+                            <fieldset className="fieldset">
+
+                                <label className=" text-lg font-semibold">Your Name</label>
+
+                                <input type="text" className="input" placeholder="Enter your name"
+                                    {...register("name", { required: true })}
+                                />
+
+                                {errors.text && (
+                                    <p className="text-red-500 text-sm">
+                                        Email is required
+                                    </p>
+                                )}
+
+
+
+                                <label className=" text-lg font-semibold">Photo URL</label>
+
+                                <input type="text" className="input" placeholder="Enter your photo url"
+                                    {...register("url", { required: true })}
+                                />
+
+                                {errors.text && (
+                                    <p className="text-red-500 text-sm">
+                                        Email is required
+                                    </p>
+                                )}
+
+
+
+
+                                <label className=" text-lg font-semibold">Email address</label>
+
+                                <input type="email" className="input" placeholder="Email"
+                                    {...register("email", { required: true })}
+                                />
+
+                                {errors.email && (
+                                    <p className="text-red-500 text-sm">
+                                        Email is required
+                                    </p>
+                                )}
+
+                                <label className="text-lg font-semibold">Password</label>
+
+                                <input type="password" className="input" placeholder="Enter your password"
+                                    {...register("password", { required: true })}
+
+                                />
+
+                                {errors.password && (
+                                    <p className="text-red-500 text-sm">
+                                        Password is required
+                                    </p>
+                                )}
+
+                                {/* <div><a className="link link-hover">Forgot password?</a></div> */}
+
+                                <div className='flex gap-2 items-center mt-2'>
+                                    <input type="checkbox" defaultChecked className="checkbox" /> 
+                                    <p>Accept Term & Conditions</p>
+                                </div>
+                                <button className="btn btn-neutral mt-4">Register</button>
+                            </fieldset>
+                        </div>
+
+                        
+                    </div>
+                </div>
+            </form>
+
         </div>
     );
 };
 
-export default RegisterPage;
+export default LoginPage;
