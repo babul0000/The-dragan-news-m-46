de96b387a176eb66027a82d858e9ce4f1ed60613
Your Name <babulhossan.info@gmail.com>
2026-04-25 10:32:55 +0600

    right sidebar

--- src/components/homesection/AllCategory.jsx
+++ src/components/homesection/AllCategory.jsx
@@ -1,30 +1,29 @@
+import Link from 'next/link';
 import React from 'react';
 
-const category = async () => {
-    const res = await fetch("https://openapi.programming-hero.com/api/news/categories")
-    const data = await res.json()
-    return data;
-}
 
 
-const AllCategory = async() => {
-    const text = await category();
-    const value = text.data.news_category;
-    console.log(value);
-    
-    
+
+const AllCategory = async ({ categories, active }) => {
+
+
+
     return (
         <div>
             <h2 className="font-bold mt-3 ml-3.5">All Category</h2>
 
             <div className="flex flex-col gap-2 p-4">
                 {
-                    value.map(val => (
+                    categories.map(val => (
                         <span
                             key={val.category_id}
-                            className="bg-white shadow-sm rounded-md px-3 py-2 text-sm text-center  "
+                            className={`${active === val.category_id ? "bg-gray-400 text-white" : "bg-white"
+                                } shadow-sm rounded-md px-3 py-2 text-sm text-center`}
                         >
-                            {val.category_name}
+                            <Link href={`/Category/${val.category_id}`}>
+                                {val.category_name}
+                            </Link>
+
                         </span>
                     ))
                 }
--- src/components/homesection/LoginSection.jsx
+++ src/components/homesection/LoginSection.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import Image from "next/image";
+import { FaGithub, FaGoogle } from "react-icons/fa";
 
-const LoginSection = () => {
+
+const RightSidebar = () => {
     return (
-        <div>
-            LoginSection.jsx 
+        <div className="space-y-6">
+
+            {/* Login */}
+            <div className="bg-white p-4 rounded-xl shadow-sm">
+                <h3 className="font-semibold mb-3">Login With</h3>
+
+                <button className="w-full text-blue-500 border rounded-md py-2 flex items-center justify-center gap-2 text-sm hover:bg-gray-100 transition">
+                    <FaGoogle /> Login with Google
+                </button>
+
+                <button className="w-full  border rounded-md py-2 flex items-center justify-center gap-2 text-sm mt-2 hover:bg-gray-100 transition">
+                    <FaGithub /> Login with Github
+                </button>
+            </div>
+
+            {/* Social */}
+            <div className="bg-white p-4 rounded-xl shadow-sm">
+                <h3 className="font-semibold mb-3">Find Us On</h3>
+
+                <div className="border rounded-md overflow-hidden">
+                    <p className="p-3 border-b hover:bg-gray-50 cursor-pointer">👍 Facebook</p>
+                    <p className="p-3 border-b hover:bg-gray-50 cursor-pointer">🐦 Twitter</p>
+                    <p className="p-3 hover:bg-gray-50 cursor-pointer">📸 Instagram</p>
+                </div>
+            </div>
+
+            
+
         </div>
     );
 };
 
-export default LoginSection;
+export default RightSidebar;
