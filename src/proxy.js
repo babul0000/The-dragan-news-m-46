045efabf1408d68b79ddd8fe617c4854d0545d7b
Your Name <babulhossan.info@gmail.com>
2026-04-26 21:27:43 +0600

    proxy setup

---
+++ src/proxy.js
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server'
+import { auth } from './lib/auth';
+import { headers } from 'next/headers';
+
+
+export async function proxy(request) {
+
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+
+
+if(session) {
+return NextResponse.next()
+}
+    return NextResponse.redirect(new URL('/login', request.url))
+
+
+    
+    // return NextResponse.redirect(new URL('/home', request.url))
+}
+
+
+
+export const config = {
+    matcher: '/career',
+}
+
+// /about/:path*
